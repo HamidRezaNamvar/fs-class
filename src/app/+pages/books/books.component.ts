2b2d5c5f378e766885a8e97fe50a03eaccbe700c
HamidReza Namvar <h.rnamvar1384@gmail.com>
2024-03-25 22:11:22 +0330

    tamrin class fs

---
+++ src/app/+pages/books/books.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+
+@Component({
+  selector: 'app-books',
+  standalone: true,
+  imports: [MatTableModule],
+  templateUrl: './books.component.html',
+  styleUrl: './books.component.scss'
+})
+export class BooksComponent {
+  data:Book[]=[
+    {id:1000, title:'C#' ,writer:'Me',price:1100},
+    {id:1000, title:'JS' ,writer:'You',price:1200},
+    {id:1000, title:'Python' ,writer:'He',price:1300},
+    {id:1000, title:'ASP .NET' ,writer:'She',price:1400}
+  ];
+  tableColumns:string[]=[
+    'id',
+    'title',
+    'writer',
+    'price'
+
+  ]
+}
+
+interface Book {
+  id: number;
+  title: string;
+  writer: string;
+  price: number;
+}
